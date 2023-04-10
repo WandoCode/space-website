68525e7093bc8e804743ae36f00ac323ca3e0cc0
Maxime Chirez <mchirez@gmail.com>
2023-04-10 23:13:39 +0200

    Add main typography styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,16 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    colors: {
+      'dark-blue': '#0B0D17',
+      'light-blue': '#D0D6F9',
+      'gray-400': '#979797',
+      'gray-600': '#151620',
+      white: '#FFF',
+    },
+    fontFamily: {
+      sans: ['Barlow Condensed', 'sans-serif'],
+      serif: ['Bellefair', 'serif'],
     },
   },
   plugins: [],
